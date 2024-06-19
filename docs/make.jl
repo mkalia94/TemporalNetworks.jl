a53d848601654d595c68da881031dbe4457ee043
Manu Kalia <manu@Manus-MacBook-Pro.local>
2024-06-19 18:29:50 +0200

    Workflows edited

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
-using Pkg
-Pkg.activate("../.")
+import Pkg
+Pkg.add("Documenter")
+using Documenter
+push!(LOAD_PATH,"../")
+push!(LOAD_PATH,"../src/")
 using TemporalNetworks
-using Documenter, Example
 makedocs(sitename="TemporalNetworks")
-deploydocs(;repo = "github.com/mkalia94/TemporalNetworks.jl", devbranch="main")
+deploydocs(;repo = "github.com/mkalia94/TemporalNetworks.jl.git",target = "build",branch = "gh-pages")
