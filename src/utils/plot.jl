4ffd45c3ce78dab7ff04c6ca00a4a2cc9ecf5bf9
Manu Kalia <manu@Manus-MacBook-Pro.local>
2024-06-25 16:26:52 +0200

    Eigenvalue and cut plotting functions added

--- src/utils/plot.jl
+++ src/utils/plot.jl
@@ -40,7 +40,15 @@ function edge_color!(W , T :: Int64, layer :: Int64, edge_colors :: Matrix{Int64
     edges_added(W, layer, edge_colors)
 end
 
+function plot(partition :: SpectralPartition{MultilayerGraph{T}, N}, R :: Int64; vecs = nothing, clims = nothing, kwargs...) where {T,N}
+    _plot_partition(partition, vecs = vecs, clims =clims, kwargs...), _plot_eigs(partition, R)
+end
+
 function plot(partition :: SpectralPartition{MultilayerGraph{T}, N}; vecs = nothing, clims = nothing, kwargs...) where {T,N}
+    _plot_partition(partition, vecs = vecs, clims =clims, kwargs...), _plot_eigs(partition, 20)
+end
+
+function _plot_partition(partition :: SpectralPartition{MultilayerGraph{T}, N}; vecs = nothing, clims = nothing, kwargs...) where {T,N}
 
     h = []
     vec_inds, vecs_to_plot = _vecs_to_plot(partition, vecs = vecs)
@@ -51,6 +59,24 @@ function plot(partition :: SpectralPartition{MultilayerGraph{T}, N}; vecs = noth
         push!(h,heatmap(reshape(vecs_to_plot[:,i], partition.graph.N, partition.graph.T),title="Orig. Evec $(ind)",c=cgrad(:RdBu), dpi=300, clims = (-yy,yy), kwargs...))
     end
     h
+end
+
+function _plot_eigs(partition :: SpectralPartition{MultilayerGraph{T}, N}, R) where {T <: Multiplex, N}
+    labels_eigs = zeros(R)
+    ind = findall(x->isspat(partition.evecs[:,x],partition.graph.N,partition.graph.T,0.01) == true, 1:R)
+    ind_not = [x for x in 1:R if x ∉ ind]
+    p5 = scatter(ind, partition.evals[ind], color=:red, label=L"\Lambda_{k,a}^{\rm spat}", xlabel=L"k", title=L"\textrm{Eigenvalues } \ \Lambda_{k,a} \ \textrm{ vs } \ k ", titlefontsize=10)
+    scatter!(p5, ind_not, partition.evals[ind_not], color=:black, markershape=:square, label=L"\Lambda_{k,a}^{\rm temp}", dpi=300, legend=:bottomright)
+    p5
+end
+
+function _plot_eigs(partition :: SpectralPartition{MultilayerGraph{T}, N}, R) where {T <: NonMultiplex, N}
+    labels_eigs = zeros(R)
+    ind = findall(x->isspat(partition.graph, partition.norm, partition.L_spat, partition.L_temp, partition.a, x, nothing) < 0.01, 1:R)
+    ind_not = [x for x in 1:R if x ∉ ind]
+    p5 = scatter(ind, partition.evals[ind], color=:red, label=L"\Lambda_{k,a}^{\rm spat}", xlabel=L"k", title=L"\textrm{Eigenvalues } \ \Lambda_{k,a} \ \textrm{ vs } \ k ", titlefontsize=10)
+    scatter!(p5, ind_not, partition.evals[ind_not], color=:black, markershape=:square, label=L"\Lambda_{k,a}^{\rm temp}", dpi=300, legend=:bottomright)
+    p5
 end
 
 function _vecs_to_plot(partition :: SpectralPartition{MultilayerGraph{T}, N}; vecs = nothing, clims = nothing, kwargs...) where {T <: NonMultiplex, N <: Normalization}
@@ -64,7 +90,19 @@ function _vecs_to_plot(partition :: SpectralPartition{MultilayerGraph{T}, N}; ve
 end
 
 
-function plot(spart:: SEBAPartition{MultilayerGraph{T}, N}; kwargs...) where {T, N}
+function plot(spart :: SEBAPartition{MultilayerGraph{T}, N}; kwargs...) where {T<: Multiplex, N}
+    _plot_SEBA_trend(spart, 5), _plot_SEBA(spart)
+end
+
+function plot(spart :: SEBAPartition{MultilayerGraph{T}, N}, R :: Int64; kwargs...) where {T <: Multiplex, N}
+    _plot_SEBA_trend(spart, R), _plot_SEBA(spart)
+end
+
+function plot(spart :: SEBAPartition{MultilayerGraph{T}, N}; kwargs...) where {T<: NonMultiplex, N}
+    _plot_SEBA(spart)
+end
+
+function _plot_SEBA(spart:: SEBAPartition{MultilayerGraph{T}, N}; kwargs...) where {T, N}
     h = []
     inds = 1:size(spart.vecs)[2]
     for i in inds
@@ -73,4 +111,21 @@ function plot(spart:: SEBAPartition{MultilayerGraph{T}, N}; kwargs...) where {T,
     h
 end
 
+function _plot_SEBA_trend(seba_part :: SEBAPartition{MultilayerGraph{T},N}, R) where {T <: Multiplex, N}
+    cuts_multipleR = []
+    for k in 1:R
+        s_temp = SEBAPartition(seba_part.partition, k)
+        push!(cuts_multipleR, s_temp.cuts)
+    end
 
+    cuts_full = zeros(2*R, R)
+    for i in 1:R
+        cuts_full[:,i] = [cuts_multipleR[i]; zeros(2*R-2*i)]
+    end
+    
+    p_cuts = groupedbar(cuts_full', bar_position = :dodge, bar_width=0.7, label="", dpi=300,title=L"H(\mathcal{X}_k), \ k=1 \dots 2R", xlabel=L"R",titlefontsize=10)
+    means = [sum(cuts_full[:,i])/(2*i) for i in 1:R]
+    scatter!(p_cuts, means, label="")
+    plot!(p_cuts, means, label="", c=:black)
+    p_cuts
+end
