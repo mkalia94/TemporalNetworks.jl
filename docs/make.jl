a821bb66263b390a30750374c0461af9ea8ba59b
Manu Kalia <manu@Manus-MacBook-Pro.local>
2024-07-11 11:29:47 +0200

    Docs flow updated

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
 import Pkg
 Pkg.add("Documenter")
 using Documenter
-#push!(LOAD_PATH,"../")
-#push!(LOAD_PATH,"../src/")
-#using TemporalNetworks
+push!(LOAD_PATH,"../")
+push!(LOAD_PATH,"../src/")
+using TemporalNetworks
 makedocs(sitename="TemporalNetworks")
 deploydocs(;repo = "github.com/mkalia94/TemporalNetworks.jl.git",target = "build",branch = "gh-pages")
