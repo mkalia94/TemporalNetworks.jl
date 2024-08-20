1ee2aa3527669244248323fd1d40cf72243a7303
Documenter.jl <documenter@juliadocs.github.io>
2024-08-20 14:36:33 +0000

    build based on ea07a37

---
+++ v1.0.0/nonmultiplex.jl
@@ -0,0 +1,33 @@
+using Pkg; Pkg.activate("../../.")
+using TemporalNetworks, Plots, LaTeXStrings
+
+η = 0.8
+list = [2,1]
+clusters = [[Array(1:5), Array(6:20), Array(21:25)],
+                [Array(1:10), Array(11:20)]] # you can ignore this line
+degrees = [[4,6,4],
+        [6,6]] # Ignore this line
+evolve = 1
+block = BlockGraphNonMultiplex(25, 10, list, η, clusters, degrees, evolve)
+W2 = block() |> Vector{Matrix{Float64}}
+
+
+mlgraph_nonmultiplex = MultilayerGraph(W2, connect = NonMultiplexCompressed())
+partition_nonmultiplex = SpectralPartition(mlgraph_nonmultiplex, compute_a = RayleighBalancing(3)) # Rayleigh balancing on third eigenvalue
+seba_part_nonmultiplex = SEBAPartition(partition_nonmultiplex,[2,])
+
+plot(plot(partition_nonmultiplex)[1][1:6]..., size = (700,400), dpi=300)
+savefig("figs/evecs_nonm.svg")
+
+plot(plot(partition_nonmultiplex)[2], size = (350,250), dpi=300)
+savefig("figs/evals_nonm.svg")
+
+plot(plot(seba_part_nonmultiplex)..., size=(700,400), dpi=300)
+savefig("figs/SEBA_nonm.svg")
+
+plot(seba_part_nonmultiplex,[1,2], dpi=300)
+savefig("figs/heatmap_nonm.svg")
+
+xx = leiden_slice(partition_nonmultiplex)
+heatmap(xx[2], c=cgrad([:white, :orange, :red]), size=(400,300), dpi=300)
+savefig("figs/leiden-slice-nonm.svg")
